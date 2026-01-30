0ccd7a89e68bd40a70266138e0b615fe81ee84f9
akashmadan501 <akashmadan501@gmail.com>
2026-01-30 17:49:00 +0530

    Added JWT authenticaiton middleware and protected routes

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const { MONGO_URI } = require("./env");
 
 async function connectDB() {
   await mongoose.connect(MONGO_URI);
-  console.log("🟢 MongoDB connected");
+  console.log("MongoDB connected");
 }
 
 module.exports = connectDB;
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const authController = require("../controllers/auth.controller");
+const authenticate = require("../middlewares/auth.middleware");
+
 
 const router = express.Router();
 
 router.post("/register", authController.register);
+
+router.get("/me", authenticate, (req, res) => {
+  return res.status(200).json({
+    success: true,
+    data: req.user,
+  });
+});
+
+
 router.post("/login", authController.login);
 
 module.exports = router;
